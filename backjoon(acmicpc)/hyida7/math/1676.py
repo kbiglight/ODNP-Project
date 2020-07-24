694876f8d6bc9e1795abf73c8ad6ff3d7df3986a
Brain Kim <kbiglight@gmail.com>
2020-07-24 17:59:44 +0900

    solve 1676, upload solution

---
+++ backjoon(acmicpc)/hyida7/math/1676.py
@@ -0,0 +1,26 @@
+
+# Tag : Stack, Recursion
+# no additional input files
+import sys
+
+input: () = lambda: sys.stdin.readline().strip()
+
+
+def factorial(N):
+    if N <= 1:
+        return 1
+    else:
+        return N * factorial(N - 1)
+
+
+N = int(input())
+fact = factorial(N)
+# print(fact)
+stack = list(str(fact))
+# print(stack)
+count = 0
+
+while stack.pop() == '0':
+    count += 1
+
+print(count)
